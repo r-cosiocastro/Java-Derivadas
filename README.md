370d33cb46631e928b39da8f5a6ad4ef65cc8fae
Rafael Cosio Castro <32781770+r-cosiocastro@users.noreply.github.com>
2022-11-23 00:50:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java-Derivadas
+ Proyecto simple usando la librería de Analizador de Expresiones Matemáticas en NetBeans 8.2.
---
+++ src/derivar/Derivar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package derivar;
+
+/**
+ *
+ * @author krawz
+ */
+public class Derivar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Interfaz().setVisible(true);
+    }
+}
